b542b033d6a16d1b67c370679397936e116a8225
melonybox <grandmadcow@gmail.com>
2019-05-14 17:17:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+// const writeCards = (["Ada", "Brendan", "Ali"], "birthday");
+
+
+function writeCards(gifts, event) {
+  let finCards = []
+  for (let i = 0; i < gifts.length; i++) {
+    finCards.push(`Thank you, ${gifts[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return finCards;
+}
+
+function countdown() {
+  let countdown = 10;
+  while (countdown >= 0) {
+    console.log(countdown--);
+  }
+}
