08c4adc23eef120a5837e4cfb4cf6aab8eddb6ee
Sylwia Vargas <sylwia.vargas@gmail.com>
2019-05-15 15:42:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(folks, ev) {
+  const text = []
+  for (let i = 0; i < folks.length; i++) {
+     text.push(`Thank you, ${folks[i]}, for the wonderful ${ev} gift!`)
+  }
+  return text
+}
+
+function countdown(i) {
+  let counter = i
+  while (counter >= 0) {
+    console.log(counter);
+    counter--;
+  }}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
